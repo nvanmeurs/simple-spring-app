e4f35d974017964e4bab539eb124d50c90de3c9f
Nicky van Meurs <1681034+nvanmeurs@users.noreply.github.com>
2020-02-05 16:32:54 +0100

    Implement user entity validation

--- src/main/java/com/tutorial/simple/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/tutorial/simple/exception/CustomResponseEntityExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.tutorial.simple.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
@@ -16,17 +21,32 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleUserNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                new Date(), ex.getMessage(), request.getDescription(false));
+        final var exceptionResponse = new ExceptionResponse(
+                new Date(), ex.getMessage(), Collections.singletonList(request.getDescription(false)));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                new Date(), ex.getMessage(), request.getDescription(false));
+        final var exceptionResponse = new ExceptionResponse(
+                new Date(), ex.getMessage(), Collections.singletonList(request.getDescription(false)));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected final ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        final var bindingResult = ex.getBindingResult();
+        final var validationMessages = bindingResult.getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        final var exceptionResponse = new ExceptionResponse(
+                new Date(), "Validation error", validationMessages);
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/tutorial/simple/user/User.java
+++ src/main/java/com/tutorial/simple/user/User.java
@@ -2,17 +2,23 @@ package com.tutorial.simple.user;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
-@Document
 public class User {
     @Id
     private String id;
 
+    @NotBlank(message="Name is a mandatory field")
+    @Size(min=2, max=128, message="Name should have at least 2 characters, and no more than 128 characters")
     private String name;
 
+    @NotNull(message="Birth date is a mandatory field")
+    @Past(message="Birth date can not be in the future")
     private Date birthDate;
 }
